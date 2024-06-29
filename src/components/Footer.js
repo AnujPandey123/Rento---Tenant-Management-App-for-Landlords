b5cdbf46f5461d51c2201bfb6e03b495d9efc33c
Anuj Pandey <120494847+AnujPandey123@users.noreply.github.com>
2024-06-29 16:31:54 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,8 @@ import { NavLink } from 'react-router-dom';
 
 const FooterContainer = styled.div`
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
   background-color: #fff;
   border-top: 1px solid #ccc;
@@ -38,24 +39,35 @@ const FooterIcon = styled.div`
 
 const Footer = () => (
   <FooterContainer>
-    <NavLink to="/app" activeClassName="active" style={{ textDecoration: 'none' }}>
-      <FooterIcon>
-        <FaEnvelope size={20} />
-        <div>Messages</div>
-      </FooterIcon>
-    </NavLink>
-    <NavLink to="/invoice" activeClassName="active" style={{ textDecoration: 'none' }}>
-      <FooterIcon>
-        <FaFileInvoiceDollar size={20} />
-        <div>Invoice</div>
-      </FooterIcon>
-    </NavLink>
-    <NavLink to="/survey" activeClassName="active" style={{ textDecoration: 'none' }}>
-      <FooterIcon>
-        <FaPoll size={20} />
-        <div>Survey</div>
-      </FooterIcon>
-    </NavLink>
+    <div style={{ display: 'flex', marginBottom: '10px' }}>
+      <NavLink to="/app" activeClassName="active" style={{ textDecoration: 'none' }}>
+        <FooterIcon>
+          <FaEnvelope size={20} />
+          <div>Messages</div>
+        </FooterIcon>
+      </NavLink>
+      <NavLink to="/invoice" activeClassName="active" style={{ textDecoration: 'none' }}>
+        <FooterIcon>
+          <FaFileInvoiceDollar size={20} />
+          <div>Invoice</div>
+        </FooterIcon>
+      </NavLink>
+      <NavLink to="/survey" activeClassName="active" style={{ textDecoration: 'none' }}>
+        <FooterIcon>
+          <FaPoll size={20} />
+          <div>Survey</div>
+        </FooterIcon>
+      </NavLink>
+    </div>
+    <div style={{ color: '#888', fontSize: '12px', marginTop: '5px' }}>
+      Created by Anuj Pandey
+      <br />
+      <a href="https://github.com/AnujPandey123" target="_blank" rel="noopener noreferrer">GitHub</a> |{' '}
+      <a href="https://www.linkedin.com/in/anuj-pandey-551bb6226/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+    </div>
+    <div style={{ color: '#555', fontSize: '10px', marginTop: '5px' }}>
+      © {new Date().getFullYear()} Rento App. All rights reserved.
+    </div>
   </FooterContainer>
 );
 
